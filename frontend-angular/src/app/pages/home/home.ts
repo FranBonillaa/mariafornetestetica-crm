cc7ee4b495de17d8bb2f38fc7433ff15438dd415
FranBonilla88 <fbonillavaro@gmail.com>
2026-06-20 16:20:37 +0200

    feat: add features section with highlights below hero

--- frontend-angular/src/app/pages/home/home.ts
+++ frontend-angular/src/app/pages/home/home.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar';
 import { HeroComponent } from '../../components/hero/hero';
 import { ServicesComponent } from '../../components/services/services';
+import { FeaturesComponent } from '../../components/features/features';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, HeroComponent, ServicesComponent],
+  imports: [NavbarComponent, HeroComponent, ServicesComponent, FeaturesComponent],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
